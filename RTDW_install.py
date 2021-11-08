6bae480c1dbfa24d72cf3bc11d7092835b3728af
sammmelg <93548331+sammmelg@users.noreply.github.com>
2021-11-08 12:55:15 -0700

    Add files via upload

--- RTDW_install.py
+++ RTDW_install.py
@@ -2,10 +2,6 @@ import os
 
 
 def main():
-    # Download lowell_report.py
-    print("Downloading lowell_report.py...")
-    get_lowell()
-
     # Run clock install function
     clock_install()
 
@@ -79,28 +75,6 @@ def dw_install():
 
     # Install dwagent
     os.system("sudo ./dwagent.sh")
-
-
-def get_lowell():
-    # Change directory to iStream folder
-    os.chdir("/home/pi/source/RMS/iStream")
-
-    # Clone the repo from github
-    os.system("git clone "
-              "https://sammmelg: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@github.com/sammmelg/"
-              "lowell_report/main/lowell_report.py")
-
-    # Change directory into repo folder
-    os.chdir("/home/pi/source/RMS/iStream/lowell_report")
-
-    # Move the file up to the iStream folder
-    os.system("mv lowell_report.py /home/pi/source/RMS/iStream")
-
-    # Go back to the iStream folder
-    os.chdir("/home/pi/source/RMS/iStream")
-
-    # Remove the un-needed folder
-    os.remove("/home/pi/source/RMS/lowell_report")
 
 
 def ssh_gen():
